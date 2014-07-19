08ba9d7a76b50d618defbc99f8817f71f76ef2a0
Jim C K Flaten <jckf@jckf.no>
2014-07-19 19:06:49 +0200

    Improved IRC kick command.

--- src/main/java/no/minecraft/Minecraftno/irc/commands/IRCKickCommand.java
+++ src/main/java/no/minecraft/Minecraftno/irc/commands/IRCKickCommand.java
@@ -28,46 +28,67 @@ public class IRCKickCommand implements IRCBotCommand {
     }
 
     public void handleMessage(PircBot bot, String channel, String sender, String message) {
-        String msg = "Ingen grunn oppgitt, vi trenger ikke en.";
-        for (User user : bot.getUsers(channel)) {
-            if (user.getNick().equals(sender)) {
-                if ((user.getPrefix().equalsIgnoreCase("%")) || (user.getPrefix().equalsIgnoreCase("@"))) {
-                    String[] args = message.split(" ");
-                    if (args.length > 1) {
-                        StringBuilder build = new StringBuilder();
-                        for (int i = 1; i < args.length; i++) {
-                            build.append(args[i] + " ");
-                        }
-                        msg = build.toString().trim();
-                    }
-                    Player kick = this.plugin.playerMatch(args[0]);
-                    if (kick != null) {
-                        kick.kickPlayer(msg);
-                        this.logHandler.log(this.userHandler.getUserId(user.getNick()), this.userHandler.getUserId(kick), 0, 0, msg, MinecraftnoLog.KICK);
-
-                        this.plugin.getServer().broadcastMessage(kick.getDisplayName() +
-                            ChatColor.YELLOW + " ble kicket: " +
-                            ChatColor.WHITE + msg);
-                        this.plugin.getServer().broadcastMessage("Kicket av : " +
-                            this.userHandler.getIrcToGamePrefix(user) + user.getNick());
-
-                        Double x = kick.getLocation().getX();
-                        Double y = kick.getLocation().getY();
-                        Double z = kick.getLocation().getZ();
-                        Float yaw = kick.getLocation().getYaw();
-                        Float pitch = kick.getLocation().getPitch();
-
-                        for (int i = x.intValue() - 2; i <= x.intValue() + 2; i++) {
-                            Location strike = new Location(kick.getWorld(), i, y, z, yaw, pitch);
-                            kick.getWorld().strikeLightningEffect(strike);
-                        }
-
-                        return;
-                    } else {
-                        bot.sendMessage(sender, Colors.RED + " Fant ikke brukeren du ville kicke.");
-                    }
-                }
+        // Finn User-objektet for personen som sendte kommandoen.
+        User user = null;
+        for (User test : bot.getUsers(channel)) {
+            if (test.getNick().equals(sender)) {
+                user = test;
+                break;
             }
+        }
+
+        // Better safe than sorry.
+        if (user == null)
+            return;
+
+        // Bare de vi stoler på får kicke spillere.
+        if (!user.isOwner() && !user.isAdmin() && !user.isOp() && !user.isHalfOp())
+            return;
+
+        String[] args = message.split(" ");
+
+        // Spilleren som skal kickes.
+        Player kick = this.plugin.playerMatch(args[0]);
+
+        // Kick-melding.
+        String msg = "Ingen grunn oppgitt, vi trenger ikke en.";
+        if (args.length > 1) {
+            StringBuilder build = new StringBuilder();
+            for (int i = 1; i < args.length; i++) {
+                build.append(args[i] + " ");
+            }
+            msg = build.toString().trim();
+        }
+
+        // Igjen, better safe than sorry.
+        if (kick == null) {
+            bot.sendMessage(sender, Colors.RED + " Fant ikke brukeren du ville kicke.");
+            return;
+        }
+
+        // Kick og logg.
+        kick.kickPlayer(msg);
+        this.logHandler.log(this.userHandler.getUserId(user.getNick()), this.userHandler.getUserId(kick), 0, 0, msg, MinecraftnoLog.KICK);
+
+        // La spillerne in-game få vite hva som skjer.
+        this.plugin.getServer().broadcastMessage(
+            kick.getDisplayName() +
+            ChatColor.YELLOW + " ble kicket: " +
+            ChatColor.WHITE + msg
+        );
+        this.plugin.getServer().broadcastMessage(
+            "Kicket av : " +
+            this.userHandler.getIrcToGamePrefix(user) + user.getNick()
+        );
+
+        // Lynnedslag!
+        Double x = kick.getLocation().getX();
+        Double y = kick.getLocation().getY();
+        Double z = kick.getLocation().getZ();
+
+        for (int i = x.intValue() - 2; i <= x.intValue() + 2; i++) {
+            Location strike = new Location(kick.getWorld(), i, y, z);
+            kick.getWorld().strikeLightningEffect(strike);
         }
     }
 }
