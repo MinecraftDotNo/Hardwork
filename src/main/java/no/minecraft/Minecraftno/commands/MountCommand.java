ba686b7aedec5a4614f320a96a4417a51f56fc46
edvin <hultberg@mittnett.net>
2015-01-25 00:40:32 +0100

    Detect different worlds between player and target.
    If mounting a player in a different world it can cause problems.

--- src/main/java/no/minecraft/Minecraftno/commands/MountCommand.java
+++ src/main/java/no/minecraft/Minecraftno/commands/MountCommand.java
@@ -1,6 +1,8 @@
 package no.minecraft.Minecraftno.commands;
 
 import no.minecraft.Minecraftno.Minecraftno;
+
+import org.bukkit.World.Environment;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
@@ -19,6 +21,21 @@ public class MountCommand extends MinecraftnoCommand {
         if (args.length == 1) {
             Player target = this.plugin.playerMatch(args[0]);
             if (target != null) {
+                
+                // Check if player and target is in the same world.
+                // Related: http://redmine.minecraft.no/issues/112
+                if (player.getWorld().getName().equalsIgnoreCase(target.getWorld().getName()) == false) {
+                    String friendlyWorldname = "world";
+                    if (target.getWorld().getEnvironment() == Environment.NETHER) {
+                        friendlyWorldname = "nether";
+                    } else if (target.getWorld().getName().equalsIgnoreCase("world_skylands") == true) {
+                        friendlyWorldname = "skylands";
+                    }
+                    
+                    player.sendMessage(getErrorChatColor() + "Spilleren " + target.getName() + " er i " + friendlyWorldname + " enn deg så kan ikke mounte.");
+                    return false;
+                }
+                
                 player.performCommand("inv on");
                 target.setPassenger(player);
             }
